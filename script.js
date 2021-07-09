035951d27cfd1e3c9306bcf1125ba90e9c964d6d
Daniel Lins Toqueiro <dtoqueiro@gmail.com>
2021-07-08 22:08:46 -0300

    Fetch API and Show Content

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 import { config } from "./config.js";
 
+/**
+ * API Key in the config.js FILE 
+ * 
+ * config.js sample:
+ * 
+ *  export const config = {
+    SECRET_API_KEY: "99999999-9999-9999-9999-3999999999",
+    };
+ */
+
 let apiKey = config.SECRET_API_KEY;
 
-console.log(
-  "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=" +
-    apiKey
+let myHeaders = new Headers();
+myHeaders.append("X-CMC_PRO_API_KEY", apiKey);
+
+let myInit = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default",
+};
+
+let myRequest = new Request(
+  "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map",
+  myInit
 );
 
-fetch(
-  "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=" +
-    apiKey
-)
+fetch(myRequest)
   .then((response) => {
     if (!response.ok)
       throw new Error(
@@ -19,45 +36,29 @@ fetch(
     return response.json();
   })
   .then((api) => {
-    console.log(api);
-  })
-  .catch((error) => {
-    console.error(error.message);
-  });
-
-/*
- //GET Fetch Requisition
-fetch('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=' + apiKey)
-.then((response) => {
-    if(!response.ok) throw new Error('Erro ao executar a requisição, status ' + response.status);
-    return response.json();
-})
-.then((api) => {
     var texto = "";
-           // Get 10 coins and symbols 
-           for(let i = 0; i < 10; i++){
+    // Get 10 coins, symbols and first_historical_data
+    for (let i = 0; i < 10; i++) {
+      //Show API information
+      let date = new Date(api.data[i].first_historical_data);
 
-
-
-           //Show API information
-
-              texto = texto + `
-             
-               <div class="media">
-                   <img src="coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
+      texto =
+        texto +
+        ` 
+               <div class="media col-6">
+                   <img src="coin.jpg" class="align-self-center me-3" alt="coin" width="100" height="60">
                    <div class="media-body">
-                   <h5 class="mt-2">${api.data[i].name}</h5>
-                   <p>${api.data[i].symbol}</p>
+                   <h5>${api.data[i].name}</h5>
+                   <p class="mb-0">${api.data[i].symbol}</p>
+                   <p class="mb-5">${date.toLocaleDateString()}</p>
                    </div>
                </div>
           
                `;
 
-               document.getElementById("coins").innerHTML = texto;
-               
-           }
-       })
-       .catch((error) => {
-           console.error(error.message);
-       });
-*/
+      document.getElementById("coins").innerHTML = texto;
+    }
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
